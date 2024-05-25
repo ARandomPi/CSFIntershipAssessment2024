09ba02703af45de4902ae89e74af3b3c4c91b360
ARandomPi (ZiXu Liu) <zixu.liu@mail.mcgill.ca>
2024-05-25 16:32:26 -0400

    Setting up the Spring Boot Backend

---
+++ backend/event/src/test/java/project/event/EventRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package project.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
